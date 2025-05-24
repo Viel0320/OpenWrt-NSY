a748eedfc4cfa542dabc799e199a64c4f0c27bfa
皆川すみれ <Imlonelywalker@gmail.com>
2025-05-25 00:40:05 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -2,6 +2,7 @@
 
 # 修改默认IP
 sed -i 's/192.168.5.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git;main' >> feeds.conf.default
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
